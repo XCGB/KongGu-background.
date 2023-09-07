16aa10f5718877fbd0da15dd04fdf4dc8f60f9fc
WHJ <WHJ@guigu.com>
2023-09-07 10:50:57 +0800

    优化version 0.3

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,90 +1,154 @@
--- 创建库
-create database if not exists kong_gu;
+/*
+ Navicat Premium Data Transfer
 
--- 切换库
-use kong_gu;
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80030
+ Source Host           : localhost:3306
+ Source Schema         : kong_gu
 
--- 用户表
-create table users
-(
-    id           bigint auto_increment comment '用户ID'
-        primary key,
-    userAccount  varchar(15)                        not null comment '学号（全局唯一）',
-    username     varchar(50)                        not null comment '用户姓名',
-    nickname     varchar(50)                        null comment '昵称',
-    userPassword varchar(128)                       not null comment '密码（加密存储）',
-    avatar       varchar(256)                       null comment '头像',
-    gender       tinyint  default 0                 not null comment '性别 0-男 1-女',
-    grade        varchar(256)                       null comment '入学年级',
-    college      varchar(256)                       null comment '学院',
-    profession   varchar(256)                       null comment '专业',
-    hobby        varchar(256)                       null comment '爱好',
-    userStatus   tinyint  default 0                 null comment '用户状态 0-正常 1-封号',
-    userRole     tinyint  default 0                 null comment '用户鉴权 0-普通用户 1-高级用户 2-管理用户',
-    isDelete     tinyint  default 0                 null comment '逻辑删除 0-存在 1-删除',
-    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    constraint users_uq
-        unique (userAccount)
-) comment '用户表';
+ Target Server Type    : MySQL
+ Target Server Version : 80030
+ File Encoding         : 65001
 
+ Date: 07/09/2023 10:49:18
+*/
 
--- 帖子表
-create table post
-(
-    id            bigint auto_increment comment '主键ID'
-        primary key,
-    userId        bigint                             not null comment '发帖用户ID',
-    content       text                               not null comment '发帖内容',
-    reviewStatus  tinyint  default 0                 not null comment '审核状态 0-待审核 1-通过 2-拒绝',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    thumNum       int      default 0                 not null comment '点赞数',
-    commentNum    int      default 0                 not null comment '评论数',
-    isDelete      tinyint  default 0                 not null comment '逻辑删除 ',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    constraint post_users_id_fk
-        foreign key (userId) references users (id)
-) comment '帖子表';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+-- Table structure for comment_thumbs
+-- ----------------------------
+DROP TABLE IF EXISTS `comment_thumbs`;
+CREATE TABLE `comment_thumbs`  (
+                                   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                                   `userId` bigint NOT NULL COMMENT '点赞用户ID',
+                                   `commentId` bigint NOT NULL COMMENT '评论ID',
+                                   `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                   `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                   PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '评论点赞表' ROW_FORMAT = Dynamic;
 
--- 帖子点赞表
-create table post_thumb
-(
-    id         bigint auto_increment comment '主键ID'
-        primary key,
-    userId     bigint                             not null comment '点赞用户ID',
-    postId     bigint                             not null comment '帖子ID',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-) comment '帖子点赞表';
+-- ----------------------------
+-- Table structure for follow
+-- ----------------------------
+DROP TABLE IF EXISTS `follow`;
+CREATE TABLE `follow`  (
+                           `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                           `userId` bigint NOT NULL COMMENT '用户ID',
+                           `followUserId` bigint NOT NULL COMMENT '关注用户ID',
+                           `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                           `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                           `logicDelete` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除 0- 1',
+                           PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '关注表' ROW_FORMAT = Dynamic;
 
--- 标签表
-create table tag
-(
-    id         bigint auto_increment comment '主键ID'
-        primary key,
-    tagName    varchar(256)                       not null comment '标签名',
-    tagColor   varchar(255)                       not null comment '标签颜色',
-    userId     bigint                             not null comment '创建用户ID',
-    postNum    bigint   default 0                 not null comment '引用次数',
-    isDelete   int      default 0                 not null comment '逻辑删除 0-1',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
-) comment '标签表';
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post`  (
+                         `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                         `userId` bigint NOT NULL COMMENT '发帖用户ID',
+                         `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发帖内容',
+                         `reviewStatus` tinyint NOT NULL DEFAULT 0 COMMENT '审核状态 0-待审核 1-通过 2-拒绝',
+                         `reviewMessage` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '审核信息',
+                         `thumbNum` int NOT NULL DEFAULT 0 COMMENT '点赞数',
+                         `commentsNum` int NOT NULL DEFAULT 0 COMMENT '浏览数',
+                         `isDelete` tinyint NOT NULL DEFAULT 0 COMMENT '逻辑删除 ',
+                         `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                         `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                         PRIMARY KEY (`id`) USING BTREE,
+                         INDEX `post_users_id_fk`(`userId`) USING BTREE,
+                         CONSTRAINT `post_users_id_fk` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子表' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for post_comments
+-- ----------------------------
+DROP TABLE IF EXISTS `post_comments`;
+CREATE TABLE `post_comments`  (
+                                  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                                  `userId` bigint NOT NULL COMMENT '评论用户ID',
+                                  `postId` bigint NOT NULL COMMENT '帖子ID',
+                                  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '评论内容',
+                                  `thumbNum` int NULL DEFAULT 0 COMMENT '点赞数',
+                                  `status` tinyint NOT NULL DEFAULT 0 COMMENT '状态 0：正常，1：被举报，2：禁止查看',
+                                  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子评论表' ROW_FORMAT = Dynamic;
 
--- 帖子标签关联表
-create table post_tag
-(
-    id         bigint auto_increment comment '主键ID'
-        primary key,
-    postId     bigint                             not null comment '帖子ID',
-    tagId      bigint                             null comment '标签ID',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
-    constraint post_tag_post_id_fk
-        foreign key (postId) references post (id),
-    constraint post_tag_tag_id_fk
-        foreign key (tagId) references tag (id)
-) comment '帖子标签关联表';
+-- ----------------------------
+-- Table structure for post_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `post_tag`;
+CREATE TABLE `post_tag`  (
+                             `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                             `postId` bigint NOT NULL COMMENT '帖子ID',
+                             `tagId` bigint NULL DEFAULT NULL COMMENT '标签ID',
+                             `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                             `updateTime` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                             PRIMARY KEY (`id`) USING BTREE,
+                             INDEX `post_tag_post_id_fk`(`postId`) USING BTREE,
+                             INDEX `post_tag_tag_id_fk`(`tagId`) USING BTREE,
+                             CONSTRAINT `post_tag_post_id_fk` FOREIGN KEY (`postId`) REFERENCES `post` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+                             CONSTRAINT `post_tag_tag_id_fk` FOREIGN KEY (`tagId`) REFERENCES `tag` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子标签关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for post_thumb
+-- ----------------------------
+DROP TABLE IF EXISTS `post_thumb`;
+CREATE TABLE `post_thumb`  (
+                               `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                               `userId` bigint NOT NULL COMMENT '点赞用户ID',
+                               `postId` bigint NOT NULL COMMENT '帖子ID',
+                               `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                               `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                               PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子点赞表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for tag
+-- ----------------------------
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`  (
+                        `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                        `tagName` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标签名',
+                        `tagColor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '标签颜色',
+                        `userId` bigint NOT NULL COMMENT '创建用户ID',
+                        `postNum` bigint NOT NULL DEFAULT 0 COMMENT '引用次数',
+                        `isDelete` int NOT NULL DEFAULT 0 COMMENT '逻辑删除 0-1',
+                        `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                        `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                        PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '标签表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+                          `id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+                          `userAccount` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '学号（全局唯一）',
+                          `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户姓名',
+                          `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '昵称',
+                          `userPassword` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码（加密存储）',
+                          `avatar` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
+                          `gender` tinyint NOT NULL DEFAULT 0 COMMENT '性别 0-男 1-女',
+                          `grade` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '入学年级',
+                          `college` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学院',
+                          `profession` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '专业',
+                          `hobby` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '爱好',
+                          `userStatus` tinyint NULL DEFAULT 0 COMMENT '用户状态 0-正常 1-封号',
+                          `userRole` tinyint NULL DEFAULT 0 COMMENT '用户鉴权 0-普通用户 1-高级用户 2-管理用户',
+                          `isDelete` tinyint NULL DEFAULT 0 COMMENT '逻辑删除 0-存在 1-删除',
+                          `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                          `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                          PRIMARY KEY (`id`) USING BTREE,
+                          UNIQUE INDEX `users_uq`(`userAccount`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
